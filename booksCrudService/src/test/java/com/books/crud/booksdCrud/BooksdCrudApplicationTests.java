1afa0044ac250c3496b22955190e6e36595faa1f
cosmodaniel88 <cosmodaniel88@gmail.com>
2026-04-19 14:22:28 -0400

    Criando teste: criação de novo cliente

--- booksCrudService/src/test/java/com/books/crud/booksdCrud/BooksdCrudApplicationTests.java
+++ booksCrudService/src/test/java/com/books/crud/booksdCrud/BooksdCrudApplicationTests.java
@@ -1,13 +1,48 @@
 package com.books.crud.booksdCrud;
 
+import com.books.crud.booksdCrud.cliente.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.time.LocalDate;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class BooksdCrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@InjectMocks
+	private ClienteService clienteService;
 
+	@Mock
+	private ClienteRepository clienteRepository;
+
+	@Mock
+	private ClienteMapper clienteMapper;
+
+	@Test
+	void deveCadastrarCliente() {
+
+		ClienteCriarDTO dto = new ClienteCriarDTO(
+				"000.000.000-00",
+				"",
+				"MarquinhosTester",
+		LocalDate.of(2000,01,1
+		));
+
+		Cliente cliente = new Cliente();
+
+		when(clienteMapper.paraEntidade(dto)).thenReturn(cliente);
+
+		clienteService.cadastrarNovoCliente(dto);
+
+		verify(clienteRepository).save(cliente);
+
+
+	}
 }
